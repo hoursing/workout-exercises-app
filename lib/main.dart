366e132aa9727b04c58c673582255dfb98b3a4d0
hoursing com <hoursingcom@hoursings-Mac-mini.local>
2021-12-07 22:42:03 +0700

    Add search bar

--- lib/main.dart
+++ lib/main.dart
@@ -45,26 +45,47 @@ class HomeScreen extends StatelessWidget {
             ),
           ),
           SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: 52,
-                        width: 52,
-                        decoration: BoxDecoration(
-                          color: Color(0xFFF2BEA1),
-                          shape: BoxShape.circle,
-                        ),
-                        child: SvgPicture.asset('assets/icons/menu.svg'),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset('assets/icons/menu.svg'),
+                    ),
+                  ),
+                  Text("Hi guys",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4 !
+                          .copyWith(fontWeight: FontWeight.w900)),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 30),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(29.5)
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        icon: SvgPicture.asset("assets/icons/search.svg"),
+                        border: InputBorder.none
                       ),
                     ),
-                  ],
-            ),
+                  )
+                ],
               ),
+            ),
           ),
         ],
       ),
